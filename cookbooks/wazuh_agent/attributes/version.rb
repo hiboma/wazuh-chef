6a7ee13e926900ac292a46b9d7a738a0558f6f5a
伊藤 洋也 <hito@pepabo.com>
2025-02-26 11:13:56 +0900

    Bump release 4.11.0

--- cookbooks/wazuh_agent/attributes/version.rb
+++ cookbooks/wazuh_agent/attributes/version.rb
@@ -3,5 +3,5 @@
 # Author:: Wazuh <info@wazuh.com
 
 default['wazuh']['major_version'] = "4.x"
-default['wazuh']['minor_version'] = "4.10"
-default['wazuh']['patch_version'] = "4.10.1"
+default['wazuh']['minor_version'] = "4.11"
+default['wazuh']['patch_version'] = "4.11.0"
